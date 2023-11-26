d49b82ce509717a94f06770c4a5afb30111697ba
Schmidi49 <erik.schmidi49@gmail.com>
2023-11-26 01:09:33 +0100

    testing with makefile structure

---
+++ engine/main.c
@@ -0,0 +1,12 @@
+/******************************************************************************
+ * @file main.c
+ * @author Erik Schmidthaler
+ *
+ * (C) Erik Schmidthaler (25.11.2023)
+ */
+
+#include <stdio.h>
+
+int main(int const argc, char const* const* const argv){
+    printf("chess is cool\n");
+}
